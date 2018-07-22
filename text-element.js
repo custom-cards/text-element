6f7c29d8a216ab73ac4a851d8b254b01d6300590
Joakim Sørensen <ludeeus@users.noreply.github.com>
2018-07-22 21:10:23 +0200

    Removed <div> to give users more control of style (#1)

--- text-element.js
+++ text-element.js
@@ -5,12 +5,7 @@ class TextElement extends HTMLElement {
     }
     this.config = config;
     const card = document.createElement('text-element');
-    this.content = document.createElement('div');
-    this.content.style.padding = '8px';
-    this.content.style.transform= 'none';
-    card.appendChild(this.content);
-    this.appendChild(card);
-    this.content.innerHTML = this.config.text;
+    this.innerHTML = this.config.text;
   }
   getCardSize() {
     return 1;
